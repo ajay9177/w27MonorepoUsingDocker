fa28b92afd65d483baea8fd25c6614682643e525
ajay9177 <ajay098251@gmail.com>
2025-02-23 17:23:30 +0530

    completed the workflow

---
+++ apps/ws/index.ts
@@ -0,0 +1,22 @@
+console.log("Hello via Bun!");
+import { prismaClient } from "db/client";
+Bun.serve({
+    port:8081,
+    fetch(req,server){
+        if(server.upgrade(req)){
+            return;
+        }
+        return new Response("Upgrage failed",{status:500});
+    },
+    websocket:{
+        message(ws,message){
+            prismaClient.user.create({
+                data:{
+                    username:Math.random().toString(),
+                    password:Math.random().toString()
+                }
+            })
+            ws.send(message);
+        }
+    }
+});
